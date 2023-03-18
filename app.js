233400f5cd31ee18eb17197efedd4bf1f1ace104
NwobiaDavid <dnwobia@gmail.com>
2023-03-18 23:24:37 +0100

    google auth and bootstrap social buttons

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+// const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 const port = 3000;
@@ -30,25 +32,80 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+
+
 mongoose.connect("mongodb://127.0.0.1:27017/userDB",{useNewUrlParser: true});
 
 const userSchema= new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+// userSchema.plugin(findOrCreate);
 
 const User= mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, {
+        id: user.id,
+        username: user.username,
+        picture: user.picture
+      });
+    });
+  });
+  
+  passport.deserializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, user);
+    });
+  });
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+async function (accessToken, refreshToken, profile, done) {
+    try {
+      console.log(profile);
+      // Find or create user in your database
+      let user = await User.findOne({ googleId: profile.id });
+      if (!user) {
+        // Create new user in database
+        const username = Array.isArray(profile.emails) && profile.emails.length > 0 ? profile.emails[0].value.split('@')[0] : '';
+        const newUser = new User({
+          username: profile.displayName,
+          googleId: profile.id
+        });
+        user = await newUser.save();
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  }
+));
 
 app.get("/",(req,res)=>{
     res.render("home");
 })
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ["profile"] }));
+
+
+  app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect secrets.
+    res.redirect('/secrets');
+  });
 
 
 app.get("/login",(req,res)=>{
